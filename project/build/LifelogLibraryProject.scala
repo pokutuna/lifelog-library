3ed90db7cb021a5cf3d1b2b0d5e96807e3069342
pokutuna <popopopopokutuna@gmail.com>
2011-07-14 17:57:02 +0900

    remove testResources from watchPaths, for Trigger Execusion test

--- project/build/LifelogLibraryProject.scala
+++ project/build/LifelogLibraryProject.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 class LifelogLibraryProject(info: ProjectInfo) extends DefaultProject(info) {
-  override def compileOptions = super.compileOptions ++
-  compileOptions("-encoding", "utf8")
+  override def compileOptions = super.compileOptions ++ compileOptions("-encoding", "utf8")
+  override def watchPaths = mainSources +++ testSources +++ mainResources
 
   val scalaTest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1"
   val scalaQuery = "org.scalaquery" % "scalaquery_2.9.0" % "0.9.4"
