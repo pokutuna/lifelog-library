2f60e4147d216f8335d224181dd8eba04e9265d7
pokutuna <popopopopokutuna@gmail.com>
2011-07-16 18:25:34 +0900

    transformers to transform from LogToken classes to SensingModels

--- src/main/scala/parsing/LogToken.scala
+++ src/main/scala/parsing/LogToken.scala
@@ -1,15 +1,16 @@
 package com.pokutuna.lifelog.parsing
 
 import java.util.Date
+import com.pokutuna.lifelog.parsing.Transformer._
 
-object LogTokens {
+object LogToken {
 
   trait LogLine //root trait
 
   //detects
   trait DetectLog extends LogLine with HasAddress with HasDateTime with HasDeviceName
-  case class BtDetectLog(dateTime: Date, deviceName: String, address: String) extends DetectLog
-  case class WifiDetectLog(dateTime: Date, deviceName: String, address: String, signal: Int) extends DetectLog with HasSignal
+  case class BtDetectLog(dateTime: Date, deviceName: String, address: String) extends DetectLog with ToBtDevice with ToBtDetected
+  case class WifiDetectLog(dateTime: Date, deviceName: String, address: String, signal: Int) extends DetectLog with HasSignal with ToWifiDevice with ToWifiDetected
 
   //annotations
   trait Annotation extends LogLine
---
+++ src/main/scala/parsing/Transformer.scala
@@ -0,0 +1,37 @@
+package com.pokutuna.lifelog.parsing
+
+import com.pokutuna.lifelog.db.model.SensingModel._
+import com.pokutuna.lifelog.parsing.LogToken._
+import com.pokutuna.lifelog.util.TimeUtil
+
+object Transformer {
+
+  trait ToDeviceRecord {
+    def toDeviceRecord: DeviceRecord
+  }
+
+  trait ToBtDevice extends ToDeviceRecord {
+    self: HasAddress with HasDeviceName =>
+    def toDeviceRecord: DeviceRecord = BtDevice(address, deviceName)
+  }
+
+  trait ToWifiDevice extends ToDeviceRecord {
+    self: HasAddress with HasDeviceName =>
+    def toDeviceRecord: DeviceRecord = WifiDevice(address, deviceName)
+  }
+
+
+  trait ToDetectRecord {
+    def toDetectRecord: DetectedRecord
+  }
+
+  trait ToBtDetected extends ToDetectRecord {
+    self: HasAddress with HasDateTime =>
+    def toDetectRecord: DetectedRecord = BtDetected(address, TimeUtil.format(dateTime))
+  }
+
+  trait ToWifiDetected extends ToDetectRecord with HasSignal {
+    self: HasAddress with HasDateTime =>
+    def toDetectRecord: DetectedRecord = WifiDetected(address, TimeUtil.format(dateTime), signal)
+  }
+}
