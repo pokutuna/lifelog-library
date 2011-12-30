f83f48e279415dd37dae8496c98dc1b02fc6e106
pokutuna <popopopopokutuna@gmail.com>
2011-12-31 04:10:07 +0900

    とりあえずdeviceテーブルとtagテーブル分ける

--- src/main/resources/db/tagged_photo.sql
+++ src/main/resources/db/tagged_photo.sql
@@ -18,13 +18,23 @@ CREATE INDEX photo_org_date on simple_photos(date_time);
 DROP TABLE IF EXISTS tags;
 CREATE TABLE tags (
   id integer PRIMARY KEY AUTOINCREMENT,
+  device_id integer,
   photo_id integer,
-  address text,
-  device_type text
 );
 DROP INDEX IF EXISTS tag_photo_id;
 CREATE INDEX tag_photo_id on tags(photo_id);
-DROP INDEX IF EXISTS tag_address;
-CREATE INDEX tag_address on tags(address);
-DROP INDEX IF EXISTS tag_device_type_address;
-CREATE INDEX tag_device_type_address on tags(device_type, address);
+DROP INDEX IF EXISTS tag_ids;
+CREATE INDEX tag_ids on tags(device_id, photo_id);
+
+
+DROP TABLE IF EXISTS devices;
+CREATE TABLE devices (
+  id integer PRIMARY KEY AUTOINCREMENT,
+  address text UNIQUE,
+  device_type text,
+  nomadic boolean
+);
+DROP INDEX IF EXISTS device_address;
+CREATE INDEX device_address on devices(address);
+DROP INDEX IF EXISTS device_device_type_address;
+CREATE INDEX device_device_type_address on tags(device_type, address);
